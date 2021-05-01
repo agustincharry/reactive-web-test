4077e64ee0b049cc7d64e8875c46327cceea5048
Agustin Charry <hacharry@bancolombia.com.co>
2021-04-30 22:39:07 -0500

    Se organiza posible solución

--- src/main/java/com/agustincharry/reactivewebtest/client/services/ManagerServiceClient.java
+++ src/main/java/com/agustincharry/reactivewebtest/client/services/ManagerServiceClient.java
@@ -1,11 +1,11 @@
 package com.agustincharry.reactivewebtest.client.services;
 
 import com.agustincharry.reactivewebtest.models.Car;
+import com.agustincharry.reactivewebtest.models.Person;
 import com.agustincharry.reactivewebtest.models.PersonWithCars;
 import org.javatuples.Pair;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 @Service
 public class ManagerServiceClient {
@@ -19,19 +19,16 @@ public class ManagerServiceClient {
     }
 
     public Flux<PersonWithCars> getPeopleWithCars(){
-        Flux<Car> carFlux = carServiceClient.getAll();
-
-        Flux<PersonWithCars> result = carFlux
-                .groupBy(Car::getOwnerId)
+        Flux<Car> carsFlux = carServiceClient.getAll();
+        Flux<Pair<String, String[]>> groupedCarsFlux = carsFlux.groupBy(Car::getOwnerId)
                 .flatMap(Flux::collectList)
                 .map(carsByOwner -> {
                     String[] carIds = carsByOwner.stream().map(Car::getId).toArray(String[]::new);
                     return new Pair<>(carsByOwner.get(0).getOwnerId(), carIds);
-                }).flatMap(tuple -> {
-                    return personServiceClient.getById(tuple.getValue0())
-                            .flatMap(p -> Mono.just(new PersonWithCars(tuple.getValue0(), p.getName(), tuple.getValue1())));
                 });
 
-        return result;
+        Flux<Person> peopleFlux = groupedCarsFlux.flatMap(d -> personServiceClient.getById(d.getValue0()));
+
+        return peopleFlux.zipWith(groupedCarsFlux,(p, c)-> new PersonWithCars(p.getId(), p.getName(), c.getValue1()));
     }
 }
